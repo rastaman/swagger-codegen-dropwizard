ef62c028c791a380282e5c06a5f0f74ccec729f0
Ludovic Maître <ludovic.maitre@effervens.com>
2015-05-25 18:13:39 +0200

    Generate Application and configuration classes

--- src/main/java/io/brillo/swagger/codegen/dropwizard/DropwizardServerCodegen.java
+++ src/main/java/io/brillo/swagger/codegen/dropwizard/DropwizardServerCodegen.java
@@ -1,13 +1,18 @@
 package io.brillo.swagger.codegen.dropwizard;
 
+import java.io.File;
+
 import com.wordnik.swagger.codegen.CodegenConfig;
 import com.wordnik.swagger.codegen.CodegenType;
+import com.wordnik.swagger.codegen.SupportingFile;
 
 public class DropwizardServerCodegen extends com.wordnik.swagger.codegen.languages.JaxRSServerCodegen
 		implements CodegenConfig {
 
 	protected String servicesPackage;
 	protected String mainClass;
+	protected String applicationName;
+	protected String basePackage;
 
 	public CodegenType getTag() {
 		return CodegenType.SERVER;
@@ -23,15 +28,27 @@ public class DropwizardServerCodegen extends com.wordnik.swagger.codegen.languag
 
 	public DropwizardServerCodegen() {
 		super();
-		servicesPackage = "io.brillo.srv.core";
-		mainClass = "io.brillo.srv.BIOApplication";
+		basePackage = "io.brillo";
+		servicesPackage = basePackage + ".core";
+		applicationName = "BIO";
+		mainClass = basePackage + "." + applicationName + "Application";
+
 		init();
 	}
 
 	public void init() {
 		super.init();
+		additionalProperties.put("basePackage", basePackage);
 		additionalProperties.put("servicesPackage", servicesPackage);
 		additionalProperties.put("mainClass", mainClass);
+		additionalProperties.put("applicationName", applicationName);
+
+		supportingFiles.add(new SupportingFile("application.mustache",
+				(sourceFolder + File.separator + basePackage).replace(".", java.io.File.separator),
+				applicationName + "Application.java"));
+		supportingFiles.add(new SupportingFile("configuration.mustache",
+				(sourceFolder + File.separator + basePackage).replace(".", java.io.File.separator),
+				applicationName + "Configuration.java"));
 	}
 
 	public String getServicesPackage() {
@@ -49,6 +66,24 @@ public class DropwizardServerCodegen extends com.wordnik.swagger.codegen.languag
 
 	public void setMainClass(String mainClass) {
 		this.mainClass = mainClass;
+		init();
+	}
+
+	public String getApplicationName() {
+		return applicationName;
+	}
+
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+		init();
+	}
+
+	public String getBasePackage() {
+		return basePackage;
+	}
+
+	public void setBasePackage(String basePackage) {
+		this.basePackage = basePackage;
 		init();
 	}
 
