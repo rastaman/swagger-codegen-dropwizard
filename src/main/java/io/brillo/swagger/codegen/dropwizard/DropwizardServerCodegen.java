fe11e9864a98b0e3a6d21e6e4f4ad44c03f6ced5
Ludovic Maître <ludovic.maitre@effervens.com>
2015-05-25 17:48:24 +0200

    Improve POM, provide main class

--- src/main/java/io/brillo/swagger/codegen/dropwizard/DropwizardServerCodegen.java
+++ src/main/java/io/brillo/swagger/codegen/dropwizard/DropwizardServerCodegen.java
@@ -3,40 +3,53 @@ package io.brillo.swagger.codegen.dropwizard;
 import com.wordnik.swagger.codegen.CodegenConfig;
 import com.wordnik.swagger.codegen.CodegenType;
 
-public class DropwizardServerCodegen extends com.wordnik.swagger.codegen.languages.JaxRSServerCodegen implements CodegenConfig {
+public class DropwizardServerCodegen extends com.wordnik.swagger.codegen.languages.JaxRSServerCodegen
+		implements CodegenConfig {
 
-  protected String servicesPackage;
-  
-  public CodegenType getTag() {
-    return CodegenType.SERVER;
-  }
+	protected String servicesPackage;
+	protected String mainClass;
 
-  public String getName() {
-    return "dropwizard8";
-  }
+	public CodegenType getTag() {
+		return CodegenType.SERVER;
+	}
 
-  public String getHelp() {
-    return "Generates a Java Dropwizard 0.8/JDK8 Server application.";
-  }
+	public String getName() {
+		return "dropwizard8";
+	}
 
-  public DropwizardServerCodegen() {
-    super();
-    servicesPackage = "io.swagger.core";
-    init();
-  }
+	public String getHelp() {
+		return "Generates a Java Dropwizard 0.8/JDK8 Server application.";
+	}
 
-  public void init() {
-    super.init();
-    additionalProperties.put("servicesPackage", servicesPackage);
-  }
+	public DropwizardServerCodegen() {
+		super();
+		servicesPackage = "io.brillo.srv.core";
+		mainClass = "io.brillo.srv.BIOApplication";
+		init();
+	}
 
-  public String getServicesPackage() {
-    return servicesPackage;
-  }
+	public void init() {
+		super.init();
+		additionalProperties.put("servicesPackage", servicesPackage);
+		additionalProperties.put("mainClass", mainClass);
+	}
 
-  public void setServicesPackage(String servicesPackage) {
-    this.servicesPackage = servicesPackage;
-    init();
-  }
+	public String getServicesPackage() {
+		return servicesPackage;
+	}
+
+	public void setServicesPackage(String servicesPackage) {
+		this.servicesPackage = servicesPackage;
+		init();
+	}
+
+	public String getMainClass() {
+		return mainClass;
+	}
+
+	public void setMainClass(String mainClass) {
+		this.mainClass = mainClass;
+		init();
+	}
 
 }
